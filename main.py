a14770c36b18a8aec355b45b9b1560799e538941
yor42 <yoonch1022@naver.com>
2021-12-08 12:18:16 +0900

    fix camera output

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ import numpy as np
 import yolov4
 
 
-def draw_bbox(image, bboxes, CLASSES, show_label=True, show_confidence = True, Text_colors=(255,255,0), rectangle_colors='', tracking=False):
+def draw_bbox(image, bboxes, CLASSES, show_label=True, show_confidence=True, Text_colors=(255, 255, 0),
+              rectangle_colors='', tracking=False):
     NUM_CLASS = read_class_names()
     num_classes = len(NUM_CLASS)
     image_h, image_w, _ = image.shape
     hsv_tuples = [(1.0 * x / num_classes, 1., 1.) for x in range(num_classes)]
-    #print("hsv_tuples", hsv_tuples)
+    # print("hsv_tuples", hsv_tuples)
     colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
     colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
 
@@ -33,13 +34,13 @@ def draw_bbox(image, bboxes, CLASSES, show_label=True, show_confidence = True, T
         (x1, y1), (x2, y2) = (coor[0], coor[1]), (coor[2], coor[3])
 
         # put object rectangle
-        cv2.rectangle(image, (x1, y1), (x2, y2), bbox_color, bbox_thick*2)
+        cv2.rectangle(image, (x1, y1), (x2, y2), bbox_color, bbox_thick * 2)
 
         if show_label:
             # get text label
             score_str = " {:.2f}".format(score) if show_confidence else ""
 
-            if tracking: score_str = " "+str(score)
+            if tracking: score_str = " " + str(score)
 
             try:
                 label = "{}".format(NUM_CLASS[class_ind]) + score_str
@@ -51,16 +52,18 @@ def draw_bbox(image, bboxes, CLASSES, show_label=True, show_confidence = True, T
             (text_width, text_height), baseline = cv2.getTextSize(label, cv2.FONT_HERSHEY_COMPLEX_SMALL,
                                                                   fontScale, thickness=bbox_thick)
             # put filled text rectangle
-            cv2.rectangle(image, (x1, y1), (x1 + text_width, y1 - text_height - baseline), bbox_color, thickness=cv2.FILLED)
+            cv2.rectangle(image, (x1, y1), (x1 + text_width, y1 - text_height - baseline), bbox_color,
+                          thickness=cv2.FILLED)
 
             # put text above rectangle
-            cv2.putText(image, label, (x1, y1-4), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+            cv2.putText(image, label, (x1, y1 - 4), cv2.FONT_HERSHEY_COMPLEX_SMALL,
                         fontScale, Text_colors, bbox_thick, lineType=cv2.LINE_AA)
 
     return image
 
+
 def postprocess_boxes(pred_bbox, original_image, input_size, score_threshold):
-    valid_scale=[0, np.inf]
+    valid_scale = [0, np.inf]
     pred_bbox = np.array(pred_bbox)
 
     pred_xywh = pred_bbox[:, 0:4]
@@ -98,6 +101,7 @@ def postprocess_boxes(pred_bbox, original_image, input_size, score_threshold):
     coors, scores, classes = pred_coor[mask], scores[mask], classes[mask]
 
     return np.concatenate([coors, scores[:, np.newaxis], classes[:, np.newaxis]], axis=-1)
+
 
 def nms(bboxes, iou_threshold, sigma=0.3, method='nms'):
     """
@@ -147,15 +151,16 @@ def bboxes_iou(boxes1, boxes2):
     boxes1_area = (boxes1[..., 2] - boxes1[..., 0]) * (boxes1[..., 3] - boxes1[..., 1])
     boxes2_area = (boxes2[..., 2] - boxes2[..., 0]) * (boxes2[..., 3] - boxes2[..., 1])
 
-    left_up       = np.maximum(boxes1[..., :2], boxes2[..., :2])
-    right_down    = np.minimum(boxes1[..., 2:], boxes2[..., 2:])
+    left_up = np.maximum(boxes1[..., :2], boxes2[..., :2])
+    right_down = np.minimum(boxes1[..., 2:], boxes2[..., 2:])
 
     inter_section = np.maximum(right_down - left_up, 0.0)
-    inter_area    = inter_section[..., 0] * inter_section[..., 1]
-    union_area    = boxes1_area + boxes2_area - inter_area
-    ious          = np.maximum(1.0 * inter_area / union_area, np.finfo(np.float32).eps)
+    inter_area = inter_section[..., 0] * inter_section[..., 1]
+    union_area = boxes1_area + boxes2_area - inter_area
+    ious = np.maximum(1.0 * inter_area / union_area, np.finfo(np.float32).eps)
 
     return ious
+
 
 def read_class_names():
     # loads class name from a file
@@ -165,9 +170,10 @@ def read_class_names():
             names[ID] = name.strip('\n')
     return names
 
+
 def Create_Yolo(input_size=416, channels=3, training=False):
     NUM_CLASS = len(read_class_names())
-    input_layer  = Input([input_size, input_size, channels])
+    input_layer = Input([input_size, input_size, channels])
     conv_tensors = yolov4.YOLOv3_tiny(input_layer, NUM_CLASS)
 
     output_tensors = []
@@ -179,17 +185,18 @@ def Create_Yolo(input_size=416, channels=3, training=False):
     Yolo = tf.keras.Model(input_layer, output_tensors)
     return Yolo
 
-def image_preprocess(image, target_size, gt_boxes=None):
-    ih, iw    = target_size
-    h,  w, _  = image.shape
 
-    scale = min(iw/w, ih/h)
-    nw, nh  = int(scale * w), int(scale * h)
+def image_preprocess(image, target_size, gt_boxes=None):
+    ih, iw = target_size
+    h, w, _ = image.shape
+
+    scale = min(iw / w, ih / h)
+    nw, nh = int(scale * w), int(scale * h)
     image_resized = cv2.resize(image, (nw, nh))
 
     image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0)
-    dw, dh = (iw - nw) // 2, (ih-nh) // 2
-    image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
+    dw, dh = (iw - nw) // 2, (ih - nh) // 2
+    image_paded[dh:nh + dh, dw:nw + dw, :] = image_resized
     image_paded = image_paded / 255.
 
     if gt_boxes is None:
@@ -200,7 +207,8 @@ def image_preprocess(image, target_size, gt_boxes=None):
         gt_boxes[:, [1, 3]] = gt_boxes[:, [1, 3]] * scale + dh
         return image_paded, gt_boxes
 
-capture = cv2.VideoCapture(1)
+
+capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 416)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 416)
 
@@ -233,9 +241,8 @@ while True:
                 name = NUM_CLASS[class_ind]
                 action.append(name)
 
-
             if "STOP" in action:
-                #정지
+                # 정지
                 print("stop")
             elif "LEFT" in action:
                 if "RIGHT" in action:
@@ -264,8 +271,10 @@ while True:
                 print("forward")
             elif "BACKWARD" in action:
                 print("backward")
-        else:
-            print("Unable to capture image")
+        if cv2.waitKey(1) & 0xFF == 27:
             break
+    else:
+        print("Unable to capture image")
+        break
 
 capture.release()
